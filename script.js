6ab86cab2131b944b0cc310d7c82fc2c6eacdf79
Anekenonso <okenwanonsokenneth@gmail.com>
2020-04-01 21:21:10 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const navbar = document.querySelector('.navbar');
+document.addEventListener('scroll', () => {
+    if (window.top.scrollY > 19) {
+        navbar.classList.add('scroll');
+    }
+    else {
+        navbar.classList.remove('scroll');
+        navbar.style.transition = '.4s ease';
+    }
+})
